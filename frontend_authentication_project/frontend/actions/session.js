6b85e82e3efcf9c2076a8d2c3f70111eac0602b9
joe-delaney <joseph.delaney546@gmail.com>
2022-04-19 09:38:19 -0400

    finished phase 2

---
+++ frontend_authentication_project/frontend/actions/session.js
@@ -0,0 +1,22 @@
+import * as sessionUtil from "../utils/session";
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+
+export const receiveCurrentUser = (user) => ({
+    type: RECEIVE_CURRENT_USER,
+    user
+});
+
+export const logoutCurrentUser = () => ({
+    type: LOGOUT_CURRENT_USER
+})
+
+export const createNewUser = (formUser) => (dispatch) => sessionUtil.postUser(formUser)
+    .then(user => dispatch(receiveCurrentUser(user)));
+
+export const login = (formUser) => (dispatch) => sessionUtil.postSession(formUser)
+    .then(user => dispatch(receiveCurrentUser(user)));
+
+export const logout = () => dispatch => sessionUtil.deleteSession() 
+    .then(() => dispatch(logoutCurrentUser()));
