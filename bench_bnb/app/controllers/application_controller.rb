2605fbedd85266693a25de6b4bcc8719f186a5f2
joe-delaney <joseph.delaney546@gmail.com>
2022-04-19 20:07:17 -0400

    added require_logged_in and require_logged_out

--- bench_bnb/app/controllers/application_controller.rb
+++ bench_bnb/app/controllers/application_controller.rb
@@ -18,4 +18,18 @@ class ApplicationController < ActionController::Base
     session[:session_token] = nil
     current_user = nil
   end
+
+  def redirect_if_logged_in
+    redirect_to root_url if logged_in?
+  end
+
+  def redirect_if_not_logged_in
+    redirect_to new_session_url unless logged_in?
+  end
+
+  def deny_access_if_not_logged_in
+    unless logged_in?
+      render json: ['You must be logged in to do that'], status: :unauthorized
+    end
+  end
 end
